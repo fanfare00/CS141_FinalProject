e5e421a2eabd91b54a505ae1c5c29027ff2aa2cb
fanfare00 <blizzard1445@gmail.com>
2016-11-19 00:26:40 -0800

    allow changing between move and look commands

--- src/edu/cpp/cs/cs141/final_project/Utilities/InputHandler.java
+++ src/edu/cpp/cs/cs141/final_project/Utilities/InputHandler.java
@@ -10,20 +10,34 @@ public class InputHandler {
 	private Command key_D;
 	private Command key_Z;
 	private Command key_C;
+	private Command key_M;
 	
 	private Actor actor;
 	
 	
 	public InputHandler(Actor actor){
 		this.actor = actor;
-		bindCommands();
+		
 	}
 	
-	private void bindCommands() {
+	private void bindInitialCommands() {
+		
+		
+		bindMoveCommands();
+	}
+	
+	public void bindMoveCommands() {
 		key_W = new MoveCommand(actor, 1, 0);
 		key_A = new MoveCommand(actor, 0, -1);
 		key_S = new MoveCommand(actor, -1, 0);
 		key_D = new MoveCommand(actor, 0, 1);
+	}
+	
+	public void bindLookCommands() {
+		key_W = new LookCommand(actor, 1, 0);
+		key_A = new LookCommand(actor, 0, -1);
+		key_S = new LookCommand(actor, -1, 0);
+		key_D = new LookCommand(actor, 0, 1);
 	}
 	
 	public void handleInput(){	
