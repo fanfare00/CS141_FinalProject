d90cd17ec359c7d71ac644dc2e3f976e369b7ee7
fanfare00 <blizzard1445@gmail.com>
2016-11-20 01:39:23 -0800

    updated menu state

--- src/edu/cpp/cs/cs141/final_project/User_Interface/TextUI/States/MenuState.java
+++ src/edu/cpp/cs/cs141/final_project/User_Interface/TextUI/States/MenuState.java
@@ -1,18 +1,26 @@
 package edu.cpp.cs.cs141.final_project.User_Interface.TextUI.States;
 
 import edu.cpp.cs.cs141.final_project.Application;
-import edu.cpp.cs.cs141.final_project.Commands.LookCommand;
-import edu.cpp.cs.cs141.final_project.User_Interface.TextUI.Utilities.Key;
+import edu.cpp.cs.cs141.final_project.Commands.*;
 
 public class MenuState extends TextUIState{
 	
 	public MenuState(Application app) {
 		super(app);
-		key_1.setText("1 - Save Game");
-		//key_1.setCommand(command);
 		
-		key_2.setText("2 - Load Game");
-		key_3.setText("3 - New Game");
-		key_4.setText("4 - Quit Game");
+		key_1.setText("Save Game");
+		key_1.setCommand(new MenuSaveCommand());
+		
+		key_2.setText("Load Game");
+		key_2.setCommand(new MenuLoadCommand());
+		
+		key_3.setText("New Game");
+		key_3.setCommand(new MenuNewCommand());
+		
+		key_4.setText("Quit Game");
+		key_4.setCommand(new MenuQuitCommand());
+		
+		key_M.setText("Back");
+		key_M.setCommand(new ToggleMoveCommand());
 	}
 }
