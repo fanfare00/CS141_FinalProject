66ed58049d85777bcf0d4fa464ee42d9bbea45b8
fanfare00 <blizzard1445@gmail.com>
2016-11-29 01:15:15 -0800

    added powerup icons

--- src/edu/cpp/cs/cs141/final_project/User_Interface/GUI/Models/AmmoModel.java
+++ src/edu/cpp/cs/cs141/final_project/User_Interface/GUI/Models/AmmoModel.java
@@ -2,17 +2,20 @@ package edu.cpp.cs.cs141.final_project.User_Interface.GUI.Models;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
 
 public class AmmoModel implements Model{
 	@Override
 	public void draw(int x, int y, int width, int height, Graphics2D g) {
-		int innerWidth = width/4;
-		int innerHeight = height/4;
+		int innerWidth = 20;
+		int innerHeight = 20;
 		
-		int innerX = x + (width/2) - innerWidth/2;
-		int innerY = y + (height/2) - innerHeight/2;
+		int innerX = x + (width/4) - innerWidth/2;
+		int innerY = y + (height - 10) - innerHeight/2;
 
-		g.setColor(Color.MAGENTA);
-		g.fillOval(innerX, innerY, innerWidth, innerHeight);
+		//g.setColor(Color.MAGENTA);
+		//g.fillOval(innerX, innerY, innerWidth, innerHeight);
+		
+		g.drawImage(Toolkit.getDefaultToolkit().getImage("res/bullet.png"), innerX, innerY, innerWidth, innerHeight, null);
 	}
 }
